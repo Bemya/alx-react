c82e041b8dd420d618c3cc0c394b8a139422a066
Bemya <99530208+Bemya@users.noreply.github.com>
2023-06-03 22:53:22 +0300

    Update index.js

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -4,6 +4,11 @@
 /*jQuery is imported and assigned to the variable*/
 const $ = require('jquery');
 
+/*A <p> element with the text "Holberton Dashboard" is appended to the <body> element.*/
 $('body').append('<p>Holberton Dashboard</p>');
+
+/*A <p> element with the text "Dashboard data for the students" is appended to the <body> element.*/
 $('body').append('<p>Dashboard data for the students</p>');
+
+/*A <p> element with the text "Copyright - Holberton School" is appended to the <body> element.*/
 $('body').append('<p>Copyright - Holberton School</p>');
